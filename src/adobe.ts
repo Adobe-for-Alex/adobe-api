d9a1037c71453b1a776e953214e920b9286fbcc5
Chamber6821 <vania.osinin@yandex.ru>
2024-11-29 03:33:11 +0300

    Fix adobe

--- src/adobe.ts
+++ src/adobe.ts
@@ -20,13 +20,26 @@ export type LicenceGroup = {
   id: LicenseGroupId
 }
 
-export const organizations = async (token: Token): Promise<Organization[]> =>
-  (await fetch('https://bps-il.adobe.io/jil-api/v2/organizations/?include=delegation_groups_migration_status%2Crenga_tags%2Crso_values', {
+export const actualToken = async (token: Token): Promise<boolean> => {
+  return (await fetch('https://bps-il.adobe.io/jil-api/v2/organizations/?include=delegation_groups_migration_status%2Crenga_tags%2Crso_values', {
     headers: {
       'Authorization': `Bearer ${token}`,
       'X-Api-Key': 'ONESIE1',
     }
-  }).then(x => x.json())).map((x: any): Organization => ({ id: x.id }))
+  })).ok
+}
+
+export const organizations = async (token: Token): Promise<Organization[]> => {
+  const response = await fetch('https://bps-il.adobe.io/jil-api/v2/organizations/?include=delegation_groups_migration_status%2Crenga_tags%2Crso_values', {
+    headers: {
+      'Authorization': `Bearer ${token}`,
+      'X-Api-Key': 'ONESIE1',
+    }
+  })
+  const data = await response.json()
+  if (response.ok) return data.map((x: any): Organization => ({ id: x.id }))
+  throw new Error(`Failed to get organization: ${response.status} ${response.statusText} ${JSON.stringify(data)}`)
+}
 
 export const products = async (token: Token, id: OrganizationId): Promise<Product[]> =>
   (await fetch(`https://bps-il.adobe.io/jil-api/v2/organizations/${id}/products/?include_cancellation_data=true&include_created_date=true&include_expired=true&include_groups_quantity=false&include_inactive=false&include_license_activations=true&include_license_allocation_info=false&include_pricing_data=true&includeAcquiredOfferIds=false&includeConfiguredProductArrangementId=false&includeLegacyLSFields=false&processing_instruction_codes=global_administration%2Cadministration`, {
@@ -34,12 +47,14 @@ export const products = async (token: Token, id: OrganizationId): Promise<Produc
       'Authorization': `Bearer ${token}`,
       'X-Api-Key': 'ONESIE1',
     }
-  }).then(x => x.json())).map((x: any): Product => ({
-    id: x.id,
-    maxDelegations: x.cancellation.cancellableQuantity,
-    delegations: x.cancellation.remainingDelegations,
-    actual: (+new Date() - Date.parse(x.createdDate)) < (14 * 24 * 3600)
-  }))
+  }).then(x => x.json()))
+    .filter((x: any) => x.pricePoint === 'TRIAL_14_DAY_TWP_TEAM_DIRECT_CCT_ALL_APPS_COM')
+    .map((x: any): Product => ({
+      id: x.id,
+      maxDelegations: x.cancellation.cancellableQuantity,
+      delegations: x.cancellation.remainingDelegations,
+      actual: (+new Date() - Date.parse(x.createdDate)) < (14 * 24 * 3600 * 1000)
+    }))
 
 export const users = async (token: Token, organizationId: OrganizationId, productId: ProductId): Promise<User[]> =>
   (await fetch(`https://bps-il.adobe.io/jil-api/v2/organizations/${organizationId}/products/${productId}/users?filter_exclude_domain=techacct.adobe.com&include=PROVISIONING_STATUS&page=0&page_size=20&search_query=&sort=FNAME_LNAME&sort_order=ASC&currentPage=1&filterQuery=`, {
@@ -61,17 +76,19 @@ export const licenseGroups = async (token: Token, organizationId: OrganizationId
   }).then(x => x.json())).map((x: any): LicenceGroup => ({ id: x.id }))
 
 
-export const attach = async (
+export const attach = async ({ token, organizationId, productId, licenseGroupId, email }: {
   token: Token,
   organizationId: OrganizationId,
   productId: ProductId,
   licenseGroupId: LicenseGroupId,
   email: string
-): Promise<void> => {
-  await fetch(`https://bps-il.adobe.io/jil-api/v2/organizations/${organizationId}/users%3Abatch`, {
+}): Promise<void> => {
+  console.log(`Attach ${email} to ${organizationId}/${productId}/${licenseGroupId}`)
+  const response = await fetch(`https://bps-il.adobe.io/jil-api/v2/organizations/${organizationId}/users%3Abatch`, {
     method: 'POST',
     headers: {
       'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
       'X-Api-Key': 'ONESIE1',
     },
     body: JSON.stringify([
@@ -93,19 +110,23 @@ export const attach = async (
       }
     ])
   })
+  const data = await response.json()
+  console.log(`Attach ${email} to ${organizationId}/${productId}/${licenseGroupId}, answer: `, response.status, response.statusText, data)
+  if (!response.ok) throw new Error(`Failed to attach ${email} to ${productId}`)
 }
 
-export const detach = async (
+export const detach = async ({ token, organizationId, productId, licenseGroupId, userId }: {
   token: Token,
   organizationId: OrganizationId,
   productId: ProductId,
   licenseGroupId: LicenseGroupId,
   userId: UserId
-): Promise<void> => {
+}): Promise<void> => {
   await fetch(`https://bps-il.adobe.io/jil-api/v2/organizations/${organizationId}/users`, {
     method: 'PATCH',
     headers: {
       'Authorization': `Bearer ${token}`,
+      'Content-Type': 'application/json',
       'X-Api-Key': 'ONESIE1',
     },
     body: JSON.stringify([
