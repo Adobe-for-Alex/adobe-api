9bf91266672d0b06bdb57438647464fdb3ee5460
Chamber6821 <vania.osinin@yandex.ru>
2025-02-08 18:46:38 +0300

    Add proxy test to config

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,13 @@ export type Config = {
     freq: string,
     consumers: URL[]
   },
-  proxyList: URL
+  proxy: {
+    list: URL,
+    test: {
+      url: URL,
+      timeout: number
+    }
+  }
 }
 
 export const loadConfig = (): Config => {
@@ -12,10 +18,14 @@ export const loadConfig = (): Config => {
     SELENIUM_SERVER,
     PLACE_COUNT_NOTIFICATIONS_FREQ,
     PLACE_COUNT_NOTIFICATIONS_URLS,
-    PROXY_LIST_URL
+    PROXY_LIST_URL,
+    PROXY_TEST_URL,
+    PROXY_TEST_TIMEOUT
   } = process.env
   if (!SELENIUM_SERVER) throw new Error('SELENIUM_SERVER is undefined')
   if (!PROXY_LIST_URL) throw new Error('PROXY_LIST_URL is undefined')
+  if (!PROXY_TEST_URL) throw new Error('PROXY_TEST_URL is undefined')
+  if (!PROXY_TEST_TIMEOUT) throw new Error('PROXY_TEST_TIMEOUT is undefined')
   const optionalPart: Partial<Config> = PLACE_COUNT_NOTIFICATIONS_FREQ
     ? {
       placeCountNotification: {
@@ -26,7 +36,13 @@ export const loadConfig = (): Config => {
     : {}
   return {
     seleniumServer: new URL(SELENIUM_SERVER),
-    proxyList: new URL(PROXY_LIST_URL),
+    proxy: {
+      list: new URL(PROXY_LIST_URL),
+      test: {
+        url: new URL(PROXY_TEST_URL),
+        timeout: Number.parseInt(PROXY_TEST_TIMEOUT)
+      }
+    },
     ...optionalPart
   }
 }
