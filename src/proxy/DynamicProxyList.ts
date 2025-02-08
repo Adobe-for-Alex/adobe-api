6f09a9c9d19fa31f64e0ff6318407ba0e0abbcde
Chamber6821 <vania.osinin@yandex.ru>
2025-02-08 20:24:48 +0300

    Increase timeout if proxy test full failed

--- src/proxy/DynamicProxyList.ts
+++ src/proxy/DynamicProxyList.ts
@@ -13,28 +13,32 @@ export class DynamicProxyList implements Proxy {
     try {
       const response = await fetch(this.list)
       const proxies = await response.json() as string[]
-      const validProxies = (await Promise.all(proxies.map(async proxy => {
-        try {
-          const agent = new HttpsProxyAgent(`http://${proxy}`)
-          const controller = new AbortController()
-          setTimeout(() => controller.abort(), this.testTimeout)
-          await axios.get(this.testUrl.toString(), {
-            httpAgent: agent,
-            httpsAgent: agent,
-            signal: controller.signal,
-          })
-          console.log(`Proxy ${proxy} is valid`)
-          return proxy
-        } catch (e) {
-          console.log(`Proxy ${proxy} failed ${e}`)
-          return undefined
-        }
-      }))).filter(x => x !== undefined)
-      const firstValidProxy = validProxies[0]
-      if (firstValidProxy === undefined) throw new Error(`No valid proxies in ${proxies.join(', ')}`)
-      const [host, port] = firstValidProxy.split(':')
-      if (host === undefined || port === undefined) throw new Error(`Invalid format for proxy: ${firstValidProxy}`)
-      return { host, port: Number.parseInt(port) }
+      for (const timeout of [1, 2, 3, 4, 5].map(x => x * this.testTimeout)) {
+        console.log('Proxy test with timeout', timeout)
+        const validProxies = (await Promise.all(proxies.map(async proxy => {
+          try {
+            const agent = new HttpsProxyAgent(`http://${proxy}`)
+            const controller = new AbortController()
+            setTimeout(() => controller.abort(), timeout)
+            await axios.get(this.testUrl.toString(), {
+              httpAgent: agent,
+              httpsAgent: agent,
+              signal: controller.signal,
+            })
+            console.log(`Proxy ${proxy} is valid`)
+            return proxy
+          } catch (e) {
+            console.log(`Proxy ${proxy} failed ${e}`)
+            return undefined
+          }
+        }))).filter(x => x !== undefined)
+        const firstValidProxy = validProxies[0]
+        if (firstValidProxy === undefined) continue
+        const [host, port] = firstValidProxy.split(':')
+        if (host === undefined || port === undefined) throw new Error(`Invalid format for proxy: ${firstValidProxy}`)
+        return { host, port: Number.parseInt(port) }
+      }
+      throw new Error(`No valid proxies in ${proxies.join(', ')}`)
     } catch (e) {
       console.error('Failed to choose proxy')
       throw e
